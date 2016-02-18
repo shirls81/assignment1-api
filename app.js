b78087290d27dab1e2e8f8513dc774a3021714d5
shirls81 <blond-shirls@hotmail.com>
2016-02-17 19:30:38 -0700

    first garbage draft

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
-var reps = require('./lib/reps');
+'use strict';
+var path = require('path');
 var express = require('express');
+var reps = require('./lib/reps');
+var request = require('superagent');
+
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('view cache', false);
+
+
+app.get('/', function(req, res, next){
+  var method;
+  if (req.query.type === 'zip'){
+    method = res.allByZip;
+  } else if (req.query.type === 'name'){
+    method = reps.repsByName;
+  } else if (req.query.type === 'state'){
+  method = reps.repsByState;
+}
+
+if (method){
+  method(req.query.search, function(err, people){
+    if (err) {return next (err); }
+    res.render('index', {
+      reps: people,
+    });
+  });
+}
+
+app.listen(8000, function(){
+  console.log('Server started on port 8000');
+});
 
 app.get('/all/by-zip/:zip', function(req, res, next){
   reps.allByZip(req.params.zip, function(err, people){
@@ -35,9 +67,6 @@ app.get('/sens/by-state/:state', function(req, res, next){
     if (err) {return next(err);}
     res.json(people);
   });
-});
-app.listen(3000, function(){
-  console.log('listening yo');
 });
 
 module.exports = app;
