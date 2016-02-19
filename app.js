e03fd5d70df8975853e74c9db25ee90f80f01855
shirls81 <blond-shirls@hotmail.com>
2016-02-18 17:44:35 -0700

    final edits for part 3

--- app.js
+++ app.js
@@ -13,25 +13,25 @@ app.set('view cache', false);
 app.get('/', function(req, res, next){
   var method;
   if (req.query.type === 'zip'){
-    method = res.allByZip;
+    method = reps.allByZip;
   } else if (req.query.type === 'name'){
     method = reps.repsByName;
   } else if (req.query.type === 'state'){
-  method = reps.repsByState;
-}
-if (method){
-  method(req.query.search, function(err, people){
-    if (err) {return next (err); }
-  res.render('index', {
-    reps: people,
-    name: 'name',
-    zip: 'zip',
-    state: 'state',
-  });
+    method = reps.repsByState;
+  }
+ if (method) {
+    method(req.query.search, function(err, people) {
+      if (err) {return next(err); }
+     res.render('index', {
+       reps: people,
+       type: req.query.type,
+       search: req.query.search,
+    });
+   });
+  } else {
+ res.render('index');
+ }
 });
-}
-});
-
 app.get('/all/by-zip/:zip', function(req, res, next){
   reps.allByZip(req.params.zip, function(err, people){
     if (err) {return next(err);}
@@ -67,7 +67,7 @@ app.get('/sens/by-state/:state', function(req, res, next){
   });
 });
 
-app.listen(8000, function(){
-  console.log('Server started on port 8000');
+app.listen(3000, function () {
+  console.log('Port listening on port 3000!');
 });
 module.exports = app;
